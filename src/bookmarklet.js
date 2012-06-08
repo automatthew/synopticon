301bbfeaa630cbbec2f2bdfeeaa6fef5b2fe1a9e
Matthew King <mking@borderstylo.com>
2012-06-08 10:32:05 -0700

    wasn't using that js

--- src/bookmarklet.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  /* this could be any suitable commonjs require lib */
-  var s=document.createElement("script");
-  s.src="./build/synopticon.js";
-  document.body.appendChild(s);
-  s.onload = function () {
-    console.log("worked");
-  };
-})();
